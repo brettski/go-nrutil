aa0b45b2371e2d36f97ab30d6d1cf91453b139ce
Brett Slaski <brettski@yahoo.com>
2019-08-24 15:51:41 -0500

    refactored create config to use new set method

--- nrutil/set_configuration.go
+++ nrutil/set_configuration.go
@@ -8,24 +8,33 @@ import (
 )
 
 func createBaseYamlFile(yamlfile string) error {
-	ymlDecoded, err := yaml.Marshal(&GetBaseConfiguration().DefaultConfig)
-	if err != nil {
+
+	if err := setConfigurationInfo(&GetBaseConfiguration().DefaultConfig, yamlfile); err != nil {
 		return fmt.Errorf("Unable to marshal default config. %s", err.Error())
 	}
-
-	f, err := os.Create(yamlfile)
-	if err != nil {
-		return fmt.Errorf("Unable to create yaml file in home directory: %s", err.Error())
-	}
-	defer f.Close()
-	if _, err := f.Write(ymlDecoded); err != nil {
-		return fmt.Errorf("Error writing data to yaml file in home directory: %s", err.Error())
-	}
-
 	return fmt.Errorf(`
     The configuration yaml file was not in place.
     A new file was created at %s.
     Please add your New Relic User Admin key, base path and Synthetics monitor GUID's you wish to manage.
     Once set, run this again.
     `, yamlfile)
+}
+
+func setConfigurationInfo(configuration *Config, yamlfile string) error {
+	yamlBytes, err := yaml.Marshal(&configuration)
+	if err != nil {
+		return fmt.Errorf("Unable to marshal Config object to yaml byte array: %s", err.Error())
+	}
+
+	// Open the file, create if it doesn't exist. Overwrite current contents
+
+	f, err := os.OpenFile(yamlfile, os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return fmt.Errorf("Error opening or creating configuration file: %s", err.Error())
+	}
+	defer f.Close()
+	if _, err = f.Write(yamlBytes); err != nil {
+		return fmt.Errorf("Error writing to configuration file: %s", err.Error())
+	}
+	return nil
 }
